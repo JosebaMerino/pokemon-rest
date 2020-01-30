f3a659f09025eed871a933d29b138316345acfb0
Joseba <joseba.merino.pina@gmail.com>
2020-01-30 10:56:17 +0100

    Implemented get by name and by id

--- src/main/java/com/ipartek/formacion/controller/controller/PokemonController.java
+++ src/main/java/com/ipartek/formacion/controller/controller/PokemonController.java
@@ -3,6 +3,7 @@ package com.ipartek.formacion.controller.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -10,8 +11,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.Utilities;
 
 import com.google.gson.Gson;
+import com.ipartek.formacion.controller.utils.Utilidades;
 import com.ipartek.formacion.model.PokemonDAO;
 import com.ipartek.formacion.model.pojo.Pokemon;
 
@@ -20,10 +23,10 @@ import com.ipartek.formacion.model.pojo.Pokemon;
  */
 @WebServlet("/api/pokemon/*")
 public class PokemonController extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
-    private static PokemonDAO dao;   
-  
+    private static PokemonDAO dao;
+
 
 	/**
 	 * @see Servlet#init(ServletConfig)
@@ -47,52 +50,84 @@ public class PokemonController extends HttpServlet {
 
 		response.setContentType("application/json");
 		response.setCharacterEncoding("utf-8");
-		
+
 		super.service(request, response);
-		
-		
+
+
 	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		ArrayList<Pokemon> pokemons = (ArrayList<Pokemon>) dao.getAll();
-		
-		try( PrintWriter out = response.getWriter() ){
-			
-			Gson json = new Gson();
-			out.print( json.toJson(pokemons) );
-			out.flush();
-			
+
+		int id = -1;
+		String nombre = request.getParameter("nombre");
+		int status = 200;
+
+		Object objetoRespuesta = null;
+		try {
+			id = Utilidades.obtenerId(request.getPathInfo());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		response.setStatus(200);
-		
-		
+		List<Pokemon> pokemons;
+		if(nombre != null && nombre.length() != 0 ) {
+			pokemons = dao.getByName(nombre);
+			if(pokemons.isEmpty()) {
+				status = HttpServletResponse.SC_NO_CONTENT;
+			}
+			objetoRespuesta = pokemons;
+		} else if(id == -1) {
+			pokemons = dao.getAll();
+			if(pokemons.isEmpty()) {
+				status = HttpServletResponse.SC_NO_CONTENT;
+			}
+			objetoRespuesta = pokemons;
+		} else {
+			objetoRespuesta = dao.getById(id);
+			if(objetoRespuesta == null) {
+				status = HttpServletResponse.SC_NOT_FOUND;
+			}
+
+		}
+
+
+		response.setStatus(status);
+		if(objetoRespuesta != null) {
+			try (PrintWriter out = response.getWriter()) {
+
+				Gson json = new Gson();
+				out.print(json.toJson(objetoRespuesta));
+				out.flush();
+
+			}
+		}
+
+
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
 	}
 
 	/**
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
 	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
 	}
 
 	/**
 	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
 	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
 	}
 
 }
--- src/main/java/com/ipartek/formacion/model/PokemonDAO.java
+++ src/main/java/com/ipartek/formacion/model/PokemonDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +19,11 @@ public class PokemonDAO implements IDAO<Pokemon> {
 
 	private static PokemonDAO INSTANCE;
 
-	private final static org.apache.log4j.Logger LOG = LogManager.getLogger(PokemonDAO.class);
+	private final static Logger LOG = LogManager.getLogger(PokemonDAO.class);
+
+
+	private final String SQL_GET_BYID = "SELECT p.id 'pokemonId', p.nombre 'pokemonNombre' , h.id 'habilidadId', h.nombre 'habilidadNombre' FROM habilidad h, pokemon p, pokemon_has_habilidad phh WHERE phh.pokemonId = p.id AND phh.habilidadId = h.id AND p.id  = ? ORDER BY h.id ASC LIMIT 500;";
+	private final String SQL_GET_BYNAME = "SELECT p.id 'pokemonId', p.nombre 'pokemonNombre' , h.id 'habilidadId', h.nombre 'habilidadNombre' FROM habilidad h, pokemon p, pokemon_has_habilidad phh WHERE phh.pokemonId = p.id AND phh.habilidadId = h.id AND p.nombre LIKE ? ORDER BY p.id ASC LIMIT 500;";
 
 
 	private PokemonDAO() {
@@ -39,7 +44,6 @@ public class PokemonDAO implements IDAO<Pokemon> {
 
 		Map<Integer, Pokemon> mapPokemons = new HashMap<Integer, Pokemon>();
 
-		ArrayList<Pokemon> registros = new ArrayList<Pokemon>();
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pst = con.prepareStatement(sql);
 				ResultSet rs = pst.executeQuery() ) {
@@ -82,9 +86,34 @@ public class PokemonDAO implements IDAO<Pokemon> {
 
 	@Override
 	public Pokemon getById(int id) {
-		LOG.trace("not implemented yet");
+		Pokemon resul = null;
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pst = con.prepareStatement(SQL_GET_BYID);
+				) {
+			pst.setInt(1, id);
+			try(ResultSet rs = pst.executeQuery()) {
+				if(rs.next()) {
+					resul = new Pokemon();
+					resul.setId(rs.getInt("pokemonId"));
+					resul.setNombre(rs.getString("pokemonNombre"));
+
+					List<Habilidad> habilidades = resul.getHabilidades();
+					Habilidad habilidad;
+					do {
+						habilidad = new Habilidad();
+						habilidad.setId(rs.getInt("habilidadId"));
+						habilidad.setNombre(rs.getString("habilidadNombre"));
+						habilidades.add(habilidad);
+					} while (rs.next());
+				}
+			}
+
+		} catch (Exception e) {
+			LOG.error(e);
+		}
+
 		// TODO Auto-generated method stub
-		return null;
+		return resul;
 	}
 
 	@Override
@@ -106,6 +135,49 @@ public class PokemonDAO implements IDAO<Pokemon> {
 		LOG.trace("not implemented yet");
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public List<Pokemon> getByName(String nombreP) {
+
+		Map<Integer, Pokemon> mapPokemons = new HashMap<Integer, Pokemon>();
+		try( Connection con = ConnectionManager.getConnection();
+				PreparedStatement pst = con.prepareStatement(SQL_GET_BYNAME);) {
+
+			pst.setString(1, "%" + nombreP + "%");
+
+			try( ResultSet rs = pst.executeQuery();) {
+				while(rs.next()) {
+
+					int id = rs.getInt("pokemonId");
+					String nombre = rs.getString("pokemonNombre");
+					LOG.trace(id + " " + nombre);
+
+					Pokemon p;
+					if(mapPokemons.containsKey(id)) {
+						// Como el pokemon esta, lo obtenemos y le añadimos la habilidad
+						p = mapPokemons.get(id);
+					} else {
+						// Si el pokemon no esta, lo crea
+						p = new Pokemon();
+						p.setId(id);
+						p.setNombre(nombre);
+
+						mapPokemons.put(id, p);
+					}
+
+					Habilidad habilidad = new Habilidad();
+					habilidad.setId(rs.getInt("habilidadId"));
+					habilidad.setNombre(rs.getString("habilidadNombre"));
+
+					p.getHabilidades().add(habilidad);
+				}
+			}
+
+		} catch (Exception e) {
+			LOG.error(e);
+		}
+
+		return new ArrayList<Pokemon>(mapPokemons.values());
 	}
 
 }
